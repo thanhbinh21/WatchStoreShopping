c31a91db8f3cdcb2e25c5680711965b756b47584
TanDuy274 <phanduy27042004@gmail.com>
2025-09-30 23:11:15 +0700

    add Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from "react";
-import { SidebarItem } from "./components/SidebarItem";
-import { sideBars } from "./lib/data";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  // function onPress(isActive) {
-  //   return isActive ? "false" : "true";
-  // }
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        height: "100vh",
-        alignItems: "center",
-        gap: 4,
-      }}
-    >
-      {sideBars.map((item, index) => (
-        <SidebarItem
-          key={item.name}
-          name={item.name}
-          icon={item.icon}
-          isActive={activeIndex === index}
-          onClick={() => setActiveIndex(index)}
-        />
-      ))}
-    </div>
+    <>
+      <Sidebar />
+    </>
   );
 }
 
--- frontend/src/components/SidebarItem.jsx
+++ frontend/src/components/SidebarItem.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 export const SidebarItem = ({ icon, name, isActive, onClick }) => {
-  const bgColor = isActive ? "bg-[#4880FF] text-white" : "bg-transparent";
   const iconColor = isActive ? "text-white" : "text-black";
 
   return (
-    <div className="relative" onClick={onClick}>
-      {/* Thanh indicator bên trái */}
-      {isActive && (
-        <div className="absolute top-0 left-[-42px] h-full w-[4px] bg-[#4880FF] rounded-r-lg transition-all duration-300 ease-in-out"></div>
-      )}
-
+    <div
+      className="relative w-[192px] h-[50px] cursor-pointer flex items-center rounded-[6px] z-10"
+      onClick={onClick}
+    >
       {/* icon */}
       <i
         className={cn(
@@ -22,7 +19,8 @@ export const SidebarItem = ({ icon, name, isActive, onClick }) => {
       {/* nút */}
       <button
         className={cn(
-          `${bgColor} w-[192px] h-[50px] rounded-[6px] transition-colors duration-300 ease-in-out`
+          `w-full h-full rounded-[6px] transition-colors duration-300 ease-in-out`,
+          isActive ? "text-white" : "text-black bg-transparent"
         )}
       >
         {name}
