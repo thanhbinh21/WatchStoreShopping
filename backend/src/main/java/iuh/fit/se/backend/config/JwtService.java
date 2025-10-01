d77f0f0ae8bf34006c928223cd8c51ee03a68401
TanDuy274 <phanduy27042004@gmail.com>
2025-10-01 13:09:13 +0700

    add role in api

--- backend/src/main/java/iuh/fit/se/backend/config/JwtService.java
+++ backend/src/main/java/iuh/fit/se/backend/config/JwtService.java
@@ -18,9 +18,10 @@ public class JwtService {
 
     private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
 
-    public String generateToken(String username) {
+    public String generateToken(String username, String role) {
         return Jwts.builder()
                 .setSubject(username)
+                .claim("role", role)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 giờ
                 .signWith(key, SignatureAlgorithm.HS256)
--- backend/src/main/java/iuh/fit/se/backend/dto/LoginResponse.java
+++ backend/src/main/java/iuh/fit/se/backend/dto/LoginResponse.java
@@ -2,11 +2,26 @@ package iuh.fit.se.backend.dto;
 
 public class LoginResponse {
     private String accessToken;
+    private String role;
 
-    public LoginResponse(String accessToken) {
+    public LoginResponse(String accessToken, String role) {
         this.accessToken = accessToken;
+        this.role = role;
     }
+
     public String getAccessToken() {
         return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
