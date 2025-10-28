1d19bc7efd175bf1ca2d64360fee72d7ee476c3a
TanDuy274 <phanduy27042004@gmail.com>
2025-10-28 16:51:58 +0700

    add register route

--- backend/src/main/java/iuh/fit/se/backend/controller/AuthController.java
+++ backend/src/main/java/iuh/fit/se/backend/controller/AuthController.java
@@ -3,6 +3,7 @@ package iuh.fit.se.backend.controller;
 import iuh.fit.se.backend.config.JwtService;
 import iuh.fit.se.backend.dto.LoginRequest;
 import iuh.fit.se.backend.dto.LoginResponse;
+import iuh.fit.se.backend.dto.RegisterRequest;
 import iuh.fit.se.backend.entity.User;
 import iuh.fit.se.backend.service.UserService;
 import iuh.fit.se.backend.service.UserServiceImpl;
@@ -32,5 +33,29 @@ public class AuthController {
         String token = jwtService.generateToken(user.getUsername(), user.getRole().toString());
 
         return ResponseEntity.ok(new LoginResponse(token, user.getRole().toString(), user));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        // Kiểm tra username hoặc email đã tồn tại
+        if (userService.existsByUsername(request.getUsername())) {
+            return ResponseEntity.badRequest().body("Tên đăng nhập đã tồn tại");
+        }
+        if (userService.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body("Email đã được sử dụng");
+        }
+
+        // Tạo user mới
+        User newUser = User.builder()
+                .username(request.getUsername())
+                .password(request.getPassword()) // TODO: nên mã hóa password
+                .email(request.getEmail())
+                .fullName(request.getFullName())
+//                .role(request.getRole() != null ? request.getRole() : Role.USER)
+                .build();
+
+        userService.createUser(newUser);
+
+        return ResponseEntity.ok("Đăng ký thành công");
     }
 }
