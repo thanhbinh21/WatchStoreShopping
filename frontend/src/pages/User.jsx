635b281fd44b83efd725376518d9c5415c9586a5
Na <duongnhatanh241805@gmail.com>
2025-11-13 21:14:35 +0700

    navbar find

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
-
+import Navbar from "../components/navbar.jsx"
+import ProductList from "../components/ProductList.jsx";
+ 
 export const User = () => {
   const navigate = useNavigate();
 
@@ -9,9 +11,13 @@ export const User = () => {
     localStorage.removeItem("role");
     navigate("/login");
   };
-
+  const [products, setProducts] = useState([]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
+      <>
+        <Navbar onProductsChange={setProducts} />
+        <ProductList products={products} />
+      </>
       <div className="bg-white p-8 rounded-xl shadow-lg w-96 text-center">
         <h1 className="text-3xl font-bold mb-6 text-gray-800">
           Chào mừng đến Dashboard!
