0bcca3b7fb7a320520b0d71c1846f4cbbc1ce0da
tVhowww <nguyentrunghau7746@gmail.com>
2025-12-07 23:24:20 +0700

    feat(admin/product | cloudinary): using cloudinary upload products image

--- frontend/src/components/Admin/products/ProductTable.jsx
+++ frontend/src/components/Admin/products/ProductTable.jsx
@@ -56,9 +56,13 @@ export const ProductTable = ({
                     <img
                       src={
                         product.imageUrl
-                          ? (product.imageUrl.startsWith('http') ? product.imageUrl : `/images/products/${product.imageUrl}`)
+                          ? product.imageUrl.startsWith("http")
+                            ? product.imageUrl
+                            : `/images/products/${product.imageUrl}`
                           : product.primaryImageUrl
-                          ? (product.primaryImageUrl.startsWith('http') ? product.primaryImageUrl : `/images/products/${product.primaryImageUrl}`)
+                          ? product.primaryImageUrl.startsWith("http")
+                            ? product.primaryImageUrl
+                            : `/images/products/${product.primaryImageUrl}`
                           : productImg
                       }
                       alt={product.name}
